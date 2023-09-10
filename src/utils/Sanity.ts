3452aa938e42da84c6857e6a6545e1e237a26653
NathanSaludes <saludesnathaniel@gmail.com>
2023-09-11 05:05:43 +0800

    use system variables for sanity client config

--- src/utils/Sanity.ts
+++ src/utils/Sanity.ts
@@ -3,8 +3,8 @@ import { createClient } from "next-sanity";
 
 // initialize client
 export const client = createClient({
-	projectId: "1avgl6w2",
-	dataset: "production",
+	projectId: process.env.PROJECT_ID,
+	dataset: process.env.DATASET,
 	useCdn: true,
 	apiVersion: moment().format("YYYY-MM-DD"),
 })
