65ced8c89bde1c5f7fb04ef144255af462f52e08
NathanSaludes <saludesnathaniel@gmail.com>
2023-09-11 03:56:45 +0800

    Add category in skill schema in sanity

--- src/sanity/schemas/skills.ts
+++ src/sanity/schemas/skills.ts
@@ -7,6 +7,20 @@ const skills = {
 			name: "title",
 			type: "string",
       title: "Title"
+		},
+		{
+			name: "category",
+			type: "string",
+      title: "Category",
+			options: {
+				list: [
+					{ title: "Frontend", value: "frontend" },
+					{ title: "Backend", value: "backend" },
+					{ title: "Tool", value: "tool" },
+					{ title: "Design", value: "design" },
+					{ title: "Miscellaneous", value: "misc" }
+				]
+			}
 		}
 	]
 }
