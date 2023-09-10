23bd73e8f126d50c225a24bae3d3b48c376d4714
NathanSaludes <saludesnathaniel@gmail.com>
2023-09-11 04:05:05 +0800

    Add background and foreground color property in 'Pill' component

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,6 +1,18 @@
-const Pill: React.FC<React.PropsWithChildren> = ({ children }) => {
+interface Props {
+  backgroundColor?: string
+  foregroundColor?: string
+  children?: React.ReactNode | undefined
+}
+
+const Pill: React.FC<Props> = ({
+  backgroundColor = "bg-accent-background",
+  foregroundColor = "text-accent-foreground",
+  children,
+}) => {
   return (
-    <span className="inline-flex select-none items-center rounded-[30px] bg-accent-background px-3 py-1.5 text-[13px] font-bold leading-4 tracking-tight text-accent-foreground transition-all ease-in-out hover:brightness-90">
+    <span
+      className={`inline-flex select-none items-center rounded-[30px] px-3 py-1.5 text-[13px] font-bold leading-4 tracking-tight transition-all ease-in-out hover:brightness-90 ${backgroundColor} ${foregroundColor}`}
+    >
       {children}
     </span>
   )
