56a6d35ac2372616180ab8a382c3816fd369530f
NathanSaludes <saludesnathaniel@gmail.com>
2023-09-11 04:01:03 +0800

    Refactor 'SkillList' component and move to "Skills" folder in components

---
+++ src/components/Skills/SkillList.tsx
@@ -0,0 +1,30 @@
+import { client } from "@/utils/Sanity"
+import { TSkill } from "@/utils/types"
+import SkillGroup from "./SkillGroup"
+
+async function fetchSkills() {
+  const skills = await client.fetch<TSkill[]>(`*[_type == "skills"] | order(_createdAt asc) `, undefined, {
+    cache: "force-cache",
+  })
+  return skills
+}
+
+const SkillList: React.FC = async () => {
+  const skills = await fetchSkills()
+
+  return (
+    <>
+      {skills.length && (
+        <div className="block">
+          <SkillGroup group="frontend" skills={skills} title="Frontend" />
+          <SkillGroup group="backend" skills={skills} title="Backend" />
+          <SkillGroup group="tool" skills={skills} title="Tools" />
+          <SkillGroup group="design" skills={skills} title="Design" />
+          <SkillGroup group="misc" skills={skills} title="Others" />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default SkillList
