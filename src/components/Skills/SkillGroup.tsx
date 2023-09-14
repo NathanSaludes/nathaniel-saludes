98a5976adb87ef6e2bf9d68205a0573b591a28fb
NathanSaludes <saludesnathaniel@gmail.com>
2023-09-15 04:02:05 +0800

    Update SkillList component

--- src/components/Skills/SkillGroup.tsx
+++ src/components/Skills/SkillGroup.tsx
@@ -7,24 +7,27 @@ interface SkillGroupProps {
   title: string
   group: "frontend" | "backend" | "tool" | "design" | "misc"
 }
-const SkillGroup = ({ skills, group, title }: SkillGroupProps) => {
+const SkillGroup = ({ skills, group }: SkillGroupProps) => {
   return (
-    <div className="rounded-lg p-3 outline-1 outline-[rgba(0,0,0,0.03)] hover:cursor-pointer hover:bg-white hover:shadow-md hover:shadow-[rgba(0,0,0,0.05)] hover:outline">
-      <p className="mb-2 w-max cursor-text font-semibold leading-none text-black/30">{title}</p>
-      <div className="pills flex flex-wrap gap-x-1 gap-y-2">
-        {skills
-          .filter(({ category }) => category === group)
-          .map(({ _id, title }) => (
-            <Pill
-              foregroundColor={skillStyles[group].foreground}
-              backgroundColor={skillStyles[group].background}
-              key={_id}
-            >
-              {title}
-            </Pill>
-          ))}
-      </div>
-    </div>
+    <>
+      {skills
+        .filter(({ category }) => category === group)
+        .map(({ _id, title }) => (
+          <Pill
+            foregroundColor={skillStyles[group].foreground}
+            backgroundColor={skillStyles[group].background}
+            key={_id}
+          >
+            {title}
+          </Pill>
+        ))}
+    </>
+    // <div className="rounded-lg p-3 outline-1 outline-[rgba(0,0,0,0.03)] hover:cursor-pointer hover:bg-white hover:shadow-md hover:shadow-[rgba(0,0,0,0.05)] hover:outline">
+    //   <p className="mb-2 w-max cursor-text font-semibold leading-none text-black/30">{title}</p>
+    //   <div className="pills flex flex-wrap gap-x-1 gap-y-2">
+
+    //   </div>
+    // </div>
   )
 }
 
--- src/components/Skills/SkillList.tsx
+++ src/components/Skills/SkillList.tsx
@@ -6,24 +6,45 @@ async function fetchSkills() {
   const skills = await client.fetch<TSkill[]>(`*[_type == "skills"] | order(_createdAt asc) `, undefined, {
     cache: "force-cache",
   })
-  return skills
+
+  return skills.sort((a, b) => a.category.localeCompare(b.category))
 }
 
 const SkillList = async () => {
   const skills = await fetchSkills()
 
   return (
-    <>
+    <div className="flex gap-1 outline-dashed outline-1">
       {skills.length && (
         <div className="block">
-          <SkillGroup group="frontend" skills={skills} title="Frontend" />
-          <SkillGroup group="backend" skills={skills} title="Backend" />
-          <SkillGroup group="tool" skills={skills} title="Tools" />
-          <SkillGroup group="design" skills={skills} title="Design" />
-          <SkillGroup group="misc" skills={skills} title="Others" />
+          <SkillGroup
+            group="frontend"
+            skills={skills}
+            title="Frontend"
+          />
+          <SkillGroup
+            group="backend"
+            skills={skills}
+            title="Backend"
+          />
+          <SkillGroup
+            group="tool"
+            skills={skills}
+            title="TOol"
+          />
+          <SkillGroup
+            group="design"
+            skills={skills}
+            title="Design"
+          />
+          <SkillGroup
+            group="misc"
+            skills={skills}
+            title="Others"
+          />
         </div>
       )}
-    </>
+    </div>
   )
 }
 
